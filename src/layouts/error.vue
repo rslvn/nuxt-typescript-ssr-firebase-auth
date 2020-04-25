fdd753d66436f993971e18f4e71578efd03627d9
Resul Avan <Resul.Avan@rabobank.nl>
2020-04-26 00:03:34 +0200

    component for 403

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
   import { Component, Prop, Vue } from 'nuxt-property-decorator';
   import { NuxtError } from "@nuxt/types";
+  import Error403 from "~/components/error/Error403.vue";
   import Error404 from "~/components/error/Error404.vue";
   import Error500 from "~/components/error/Error500.vue";
 
@@ -19,6 +20,9 @@
     get errorPage() {
       if (this.error.statusCode === 404) {
         return Error404;
+      }
+      if (this.error.statusCode === 403) {
+        return Error403;
       }
       // catch everything else
       return Error500;
