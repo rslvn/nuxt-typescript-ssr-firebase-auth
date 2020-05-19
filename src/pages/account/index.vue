3119379de1f67f2cd108a4fb10489cd77f08eb7a
Resul Avan <Resul.Avan@rabobank.nl>
2020-05-19 23:39:40 +0200

    clean account/index

--- src/pages/account/index.vue
+++ src/pages/account/index.vue
@@ -1,62 +1,15 @@
 <template>
-
-  <div v-if="storedUser">
-    <div class="columns is-mobile">
-
-      <div class="column">
-        <b-field position="is-centered" grouped>
-          <ProfilePictureCard :user="storedUser"/>
-        </b-field>
-      </div>
-
-    </div>
-
-    <div class="columns is-mobile">
-
-      <div class="column">
-        <h2 class="subtitle">{{$t('card.user.title')}}</h2>
-        <ProfileInfo :user="storedUser"/>
-      </div>
-
-    </div>
-
-    <div class="columns is-mobile">
-
-      <div class="column">
-        <h2 class="subtitle">{{$t('card.linkedAccounts.title')}}</h2>
-        <ProviderList :user="storedUser"/>
-      </div>
-
-    </div>
-
-  </div>
+  <Profile/>
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'nuxt-property-decorator';
-  import { ProfileImagePlaceholder, ProfilePictureStorageRef, StateNamespace, StoredUser } from "~/types";
-  import ProfileInfo from "~/components/profile/ProfileInfo.vue";
-  import ProfilePictureCard from "~/components/profile/ProfilePictureCard.vue";
-  import ProviderList from "~/components/profile/ProviderList.vue";
+  import Profile from "~/components/profile/Profile.vue";
 
   @Component({
-    components: { ProviderList, ProfilePictureCard, ProfileInfo },
+    components: { Profile },
   })
   export default class Account extends Vue {
-
-    @StateNamespace.auth.Getter storedUser !: StoredUser;
-
-    @StateNamespace.auth.Action updateProfilePicture !: (profilePictureUrl: string) => void;
-
-    get parentFolderRef() {
-      return ProfilePictureStorageRef.folderRef
-        .replace(ProfilePictureStorageRef.parameters.userId, this.storedUser.userId)
-    }
-
-    get placeholder() {
-      return ProfileImagePlaceholder
-    }
-
   }
 </script>
 
