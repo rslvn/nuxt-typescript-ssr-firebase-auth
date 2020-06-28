336215b8115b1388be02f41868b0bc8422ea4e6e
Resul Avan <Resul.Avan@rabobank.nl>
2020-06-28 02:29:44 +0200

    profile component refactoring

--- src/components/profile/Profile.vue
+++ src/components/profile/Profile.vue
@@ -1,52 +1,9 @@
 <template>
   <div>
     <CoverPhoto :is-my-profile="isMyProfile" :auth-user="authUser" :cover-photo="user.coverPhoto"/>
-
-    <div class="container">
-      <div class="has-margin-top-10 has-margin-bottom-10">
-        <article class="media has-margin-left-5">
-          <div class="media-left">
-            <ProfilePhoto :is-my-profile="isMyProfile" :auth-user="authUser" :profile-photo="user.profilePhoto"/>
-          </div>
-          <div class="media-content">
-            <div class="content">
-              <p><strong>{{ fullName }}</strong></p>
-              <p><small>@{{ user.username }}</small></p>
-              <p class="has-margin-top-5">
-                <b-taglist attached>
-                  <b-tag :type="userPrivacyConfig.type">
-                    <b-icon
-                      :icon="userPrivacyConfig.icon"
-                      size="is-small">
-                    </b-icon>
-                    {{$t(`privacy.${userPrivacyConfig.privacyType}.title`)}}
-                  </b-tag>
-                  <b-tag type="is-light">{{$t(`privacy.${userPrivacyConfig.privacyType}.subtitle`)}}
-                    <b-tooltip :label="$t(`privacy.${userPrivacyConfig.privacyType}.description`)" multilined>
-                      <b-icon
-                        icon="information"
-                        size="is-small">
-                      </b-icon>
-                    </b-tooltip>
-                  </b-tag>
-                </b-taglist>
-              </p>
-              <!--              <p class="has-margin-top-5">-->
-              <!--                <b-tabs type="is-toggle" expanded class="has-margin-top-10" size="is-small" multiline>-->
-              <!--                  <b-tab-item label="Pictures" icon="google-photos"></b-tab-item>-->
-              <!--                  <b-tab-item label="Music" icon="library-music"></b-tab-item>-->
-              <!--                  <b-tab-item label="Videos" icon="video"></b-tab-item>-->
-              <!--                </b-tabs>-->
-              <!--              </p>-->
-            </div>
-          </div>
-        </article>
-
-      </div>
-    </div>
+    <ProfileBand :is-my-profile="isMyProfile" :auth-user="authUser" :user="user"/>
 
     <template v-if="isMyProfile">
-
       <div class="columns is-centered">
 
         <div class="column is-half">
@@ -75,9 +32,10 @@
   import ProfilePhoto from "~/components/profile/ProfilePhoto.vue";
   import CoverPhoto from "~/components/profile/CoverPhoto.vue";
   import ProviderList from "~/components/profile/ProviderList.vue";
+  import ProfileBand from '~/components/profile/ProfileBand.vue';
 
   @Component({
-    components: { CoverPhoto, ProfilePhoto, ProviderList, ProfileInfo },
+    components: { ProfileBand, CoverPhoto, ProfilePhoto, ProviderList, ProfileInfo },
   })
   export default class Profile extends Vue {
 
